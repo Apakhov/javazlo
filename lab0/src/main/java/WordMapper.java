c2029c9950547430068f67017dde6f4c21441f10
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-08 20:47:46 +0300

    first commit

---
+++ lab0/src/main/java/WordMapper.java
@@ -0,0 +1,15 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        for (String v: value.toString().split("[^\\p{L}\\p{N}-']")){
+            context.write(new Text(v.toLowerCase()), new IntWritable(1));
+        }
+    }
+}
