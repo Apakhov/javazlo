6d99092124e4955d6e67cff8dca1f4334a8bff5f
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-08 21:57:08 +0300

    Scripted auto-commit on change (2019-10-08 21:57:08) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
 
-public class AirportMapper {
+public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        for (String v: value.toString().split("[^\\p{L}\\p{N}-']")){
+            context.write(new Text(v.toLowerCase()), new IntWritable(1));
+        }
+    }
 }
--- lab2/src/main/java/FlightStatApp.java
+++ lab2/src/main/java/FlightStatApp.java
@@ -3,19 +3,20 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class FlightStatApp {
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: FlightStatApp <input path flights> <> <output path>");
+        if (args.length != 3) {
+            System.err.println("Usage: FlightStatApp <input path flights> <input path airport> <output path>");
             System.exit(-1);
         }
         Job job = Job.getInstance();
         job.setJarByClass(FlightStatApp.class);
         job.setJobName("Flight stat count");
-        MultipleInputs.addInputPath(job, new Path(args[0]));
-        MultipleInputs.addInputPath(job, new Path(args[1]));
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setReducerClass(StatReducer.class);
         job.setOutputKeyClass(Text.class);
