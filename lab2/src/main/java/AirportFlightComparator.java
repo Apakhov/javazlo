3af3f56b4a5bd894794a379ca6a0e67b80ddccdc
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-11 09:17:12 +0300

    Scripted auto-commit on change (2019-10-11 09:17:12) by gitwatch.sh

--- lab2/src/main/java/AirportFlightComparator.java
+++ lab2/src/main/java/AirportFlightComparator.java
@@ -6,7 +6,9 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
 public class AirportFlightComparator implements  WritableComparable<AirportFlightComparator> {
-    int type
+    private int enum {
+        
+    }
 
     @Override
     public void readFields(DataInput in) throws IOException {
