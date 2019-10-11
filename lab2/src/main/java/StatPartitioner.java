e1420a8dbbc3c483472028b09b24e026d7fc6fbe
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-12 00:16:38 +0300

    Scripted auto-commit on change (2019-10-12 00:16:38) by gitwatch.sh

--- lab2/src/main/java/StatPartitioner.java
+++ lab2/src/main/java/StatPartitioner.java
@@ -1,4 +1,9 @@
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapred.Partitioner;
 
-public class StatPartitioner implements Partitioner<> {
+public class StatPartitioner implements Partitioner<AirportFlightComparator, Writable> {
+    @Override
+    public int getPartition(AirportFlightComparator airportFlightComparator, Writable writable, int i) {
+        return 0;
+    }
 }
