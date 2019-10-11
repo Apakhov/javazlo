34ce4ac4fb44221f88c6d2a99ecc15a31ca7108a
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-12 02:55:56 +0300

    Scripted auto-commit on change (2019-10-12 02:55:56) by gitwatch.sh

--- lab2/src/main/java/StatReducer.java
+++ lab2/src/main/java/StatReducer.java
@@ -9,36 +9,38 @@ public class StatReducer extends Reducer<AirportFlightComparator, Text, Text, Te
     @Override
     protected void reduce(AirportFlightComparator key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
-        Float min = null;
-        Float max = null;
-        Float sum = null;
-        int cnt = 0;
-        String first = values.iterator().next().toString();
-        for (Text value : values) {
-            float cur = Float.parseFloat(value.toString());
-            if (cnt == 0){
-                min = new Float(cur);
-                max = new Float(cur);
-                sum = new Float(cur);
-                throw new IOException("not now");
-                //continue;
-            }
-            min = Math.min(min, cur);
-            max = Math.max(max, cur);
-            sum += cur;
-            cnt++;
-        }
-        //throw new IOException(sum.length())+" "+ String.valueOf(cnt) +" "+ String.valueOf(all));
-        StringBuilder sbuf = new StringBuilder();
-        Formatter fmt = new Formatter(sbuf);
-        if (cnt == 0){
-            fmt.format(
-                    "cnt: %s, max: %f, min: %f, avg: %s",
-                    cnt, max, min, sum/cnt
-            );
-        }else {
-            fmt.format("always on time!");
-        }
-        context.write(new Text(first), new Text(fmt.toString()));
+        throw new IOException("not now");
+
+//        Float min = null;
+//        Float max = null;
+//        Float sum = null;
+//        int cnt = 0;
+//        String first = values.iterator().next().toString();
+//        for (Text value : values) {
+//            float cur = Float.parseFloat(value.toString());
+//            if (cnt == 0){
+//                min = new Float(cur);
+//                max = new Float(cur);
+//                sum = new Float(cur);
+//                throw new IOException("not now");
+//                //continue;
+//            }
+//            min = Math.min(min, cur);
+//            max = Math.max(max, cur);
+//            sum += cur;
+//            cnt++;
+//        }
+//        //throw new IOException(sum.length())+" "+ String.valueOf(cnt) +" "+ String.valueOf(all));
+//        StringBuilder sbuf = new StringBuilder();
+//        Formatter fmt = new Formatter(sbuf);
+//        if (cnt == 0){
+//            fmt.format(
+//                    "cnt: %s, max: %f, min: %f, avg: %s",
+//                    cnt, max, min, sum/cnt
+//            );
+//        }else {
+//            fmt.format("always on time!");
+//        }
+//        context.write(new Text(first), new Text(fmt.toString()));
     }
 }
