b95862e24255caa18848187cfd9b9a211fc88db5
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-12 01:05:04 +0300

    Scripted auto-commit on change (2019-10-12 01:05:04) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -10,7 +10,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportFlightCompar
     protected void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
 
-        AirportID v = new Flight(value);
+        AirportID v = new AirportID(value);
         AirportFlightComparator k = new AirportFlightComparator(v.getAirportID(), 1);
         context.write(k, v);
     }
