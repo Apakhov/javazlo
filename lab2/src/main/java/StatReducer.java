2d6e912cb66fdc358f058d15a73694fbefb21b66
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-12 01:28:56 +0300

    Scripted auto-commit on change (2019-10-12 01:28:56) by gitwatch.sh

--- lab2/src/main/java/StatReducer.java
+++ lab2/src/main/java/StatReducer.java
@@ -4,28 +4,15 @@ import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
-public class StatReducer extends Reducer<AirportFlightComparator, AirportID, Text, Text> {
+public class StatReducer extends Reducer<AirportFlightComparator, Text, Text, Text> {
     @Override
-    protected void reduce(AirportFlightComparator key, Iterable<AirportID> values, Context context)
+    protected void reduce(AirportFlightComparator key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
         String sum = "";
         int cnt = 0;
         int all = 0;
-        for (AirportID value : values) {
-            if (value instanceof Airport){
-                sum += "0";
-                continue;
-            }
-            if (value instanceof Flight){
-                sum += "1";
-                continue;
-            }
-            if (value != null){
-                sum += "2";
-            }
-            if (value == null){
-                throw new IOException("garbage");
-            }
+        for (Text value : values) {
+            sum += value.toString();
         }
         //throw new IOException(sum +" "+ String.valueOf(cnt) +" "+ String.valueOf(all));
         context.write(key.getAirportID(), new Text(sum));
