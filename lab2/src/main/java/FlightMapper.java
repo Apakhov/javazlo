2928fab2049b6475a193eb8b4fa60321a20a1663
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-12 01:34:59 +0300

    Scripted auto-commit on change (2019-10-12 01:34:59) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -12,6 +12,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportFlightCompar
 
         String[] fields = value.toString().split(",");
         Text v = new Text(fields[14]+","+fields[18]);
+        throw new IOException(v.toString());
         AirportFlightComparator k = new AirportFlightComparator(new Text(fields[14]), 1);
         context.write(k, v);
     }
