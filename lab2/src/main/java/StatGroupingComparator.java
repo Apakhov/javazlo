9783b95564f2153bcf96d6f68fc9e062d0396174
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-12 00:34:58 +0300

    Scripted auto-commit on change (2019-10-12 00:34:58) by gitwatch.sh

--- lab2/src/main/java/StatGroupingComparator.java
+++ lab2/src/main/java/StatGroupingComparator.java
@@ -1,15 +1,17 @@
 import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 
-public class StatGroupingComparator implements RawComparator<AirportFlightComparator> {
+public class StatGroupingComparator extends WritableComparator {
+
+
 
     @Override
-    public int compare(byte[] bytes, int i, int i1, byte[] bytes1, int i2, int i3) {
+    public int compare(WritableComparable o1, WritableComparable o2) {
+        if (o1 instanceof AirportFlightComparator && o2 instanceof AirportFlightComparator){
+            return ((AirportFlightComparator) o1).getAirportID().compareTo(((AirportFlightComparator) o2).getAirportID());
+        }
         return 0;
-    }
-
-    @Override
-    public int compare(AirportFlightComparator o1, AirportFlightComparator o2) {
-        return o1.getAirportID().compareTo(o2.getAirportID());
     }
 }
