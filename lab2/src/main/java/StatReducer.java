5691e300cdb4696054326f9e2d271675dc0871af
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-12 02:34:22 +0300

    Scripted auto-commit on change (2019-10-12 02:34:22) by gitwatch.sh

--- lab2/src/main/java/StatReducer.java
+++ lab2/src/main/java/StatReducer.java
@@ -8,15 +8,13 @@ public class StatReducer extends Reducer<AirportFlightComparator, Text, Text, Te
     @Override
     protected void reduce(AirportFlightComparator key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
-        String sum = "";
         int cnt = 0;
-        int all = 0;
-        values.iterator().next();
+        String first = values.iterator().next().toString();
         for (Text value : values) {
             sum += value.toString();
         }
         sum += "\n\n";
         //throw new IOException(sum.length())+" "+ String.valueOf(cnt) +" "+ String.valueOf(all));
-        context.write(key.getAirportID(), new Text(sum));
+        context.write(key.getAirportID(), new Text(fi));
     }
 }
