4007d443c942ceabc9e8b2fa34c7547b20cc4ac9
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-11 09:12:00 +0300

    Scripted auto-commit on change (2019-10-11 09:12:00) by gitwatch.sh

--- lab2/src/main/java/AirportFlightComparator.java
+++ lab2/src/main/java/AirportFlightComparator.java
@@ -25,4 +25,9 @@ public class AirportFlightComparator implements  WritableComparable {
     public int hashCode(){
         return 0;
     }
+
+    @Override
+    public boolean equals(Object o){
+        
+    }
 }
