f39cd473984e8e8b4a44c0f207f41fe753d7d4bd
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-12 12:33:11 +0300

    Scripted auto-commit on change (2019-10-12 12:33:11) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -6,6 +6,10 @@ import org.apache.hadoop.mapreduce.Mapper;
         import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportFlightComparator, Text>  {
+    private static final int rowsAm = 23;
+    private static final int idRow = 14;
+    private static final int delayRow = 18;
+
     @Override
     protected void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
