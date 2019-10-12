f82da71ea6b174061dd1543a85d0c078886f41ce
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-12 12:48:48 +0300

    Scripted auto-commit on change (2019-10-12 12:48:48) by gitwatch.sh

--- lab2/src/main/java/CSVUtils.java
+++ lab2/src/main/java/CSVUtils.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class CSVUtils {
+    public static String first(String fields){
+        return fields.toString().split(",", 2)[0];
+    }
+
+    public static String second(String fields){
+        return fields.toString().split(",", 2)[1];
+    }
+
+
+    public static String[] parseFields(String fields){
+        return fields.split(",");
+    }
 }
