991d824cf996c6d7c1d37f2abeec2243632b2bb6
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-16 23:48:46 +0300

    Scripted auto-commit on change (2019-10-16 23:48:46) by gitwatch.sh

--- lab3/src/main/java/FlightStatApp.java
+++ lab3/src/main/java/FlightStatApp.java
@@ -39,7 +39,7 @@ public class FlightStatApp {
                     );
                 }
         );
-        splitted.re
+        splitted.reduce()
 //        JavaPairRDD<String, Long> wordsWithCount =
 //                splitted.mapToPair(
 //                        s -> new Tuple2<>( s, 1L)
