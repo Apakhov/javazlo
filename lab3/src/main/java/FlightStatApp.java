75a4cffaa0e28de8fd3aa42bd67952d923330f29
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-16 23:30:43 +0300

    Scripted auto-commit on change (2019-10-16 23:30:43) by gitwatch.sh

--- lab3/src/main/java/FlightStatApp.java
+++ lab3/src/main/java/FlightStatApp.java
@@ -14,39 +14,36 @@ import java.util.Arrays;
 
 public class FlightStatApp {
     public static void main(String[] args) throws Exception {
-        CSVParser p = new CSVParser("A", "B");
-        CSVRow r = p.Parse("\"10001\",\"Afognak Lake, AK: Afognak Lake Airport\"");
-        System.out.println(r.get("A")+" "+r.get("B"));
-////        if (args.length != 3) {
-////            System.err.println("Usage: FlightStatApp <input path flights> <input path airport> <output path>");
-////            System.exit(-1);
-////        }
-////        JavaRDD<String> airportsFile = sc.textFile("airports.csv");
-//        SparkConf conf = new SparkConf().setAppName("lab3");
-//        JavaSparkContext sc = new JavaSparkContext(conf);
-//        CSVParser airportParser = new CSVParser("Code","Description");
-//        JavaRDD<String> flightsFile = sc.textFile("airports.csv");
-//        JavaPairRDD splitted = flightsFile.mapToPair(
-//                s -> {
-//                    CSVRow row = airportParser.Parse(s);
-//                    return new Tuple2<>(row.get("Code")+"A", row.get("Description")+"B");
-//                }
+//        if (args.length != 3) {
+//            System.err.println("Usage: FlightStatApp <input path flights> <input path airport> <output path>");
+//            System.exit(-1);
+//        }
+//        JavaRDD<String> airportsFile = sc.textFile("airports.csv");
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        CSVParser airportParser = new CSVParser("Code","Description");
+        JavaRDD<String> flightsFile = sc.textFile("airports.csv");
+        JavaPairRDD splitted = flightsFile.mapToPair(
+                s -> {
+                    CSVRow row = airportParser.Parse(s);
+                    return new Tuple2<>(row.get("Code")+"A", row.get("Description")+"B");
+                }
+        );
+//        JavaPairRDD<String, Long> wordsWithCount =
+//                splitted.mapToPair(
+//                        s -> new Tuple2<>( s, 1L)
+//                );
+//        JavaPairRDD<String, Long> collectedWords = wordsWithCount.reduceByKey (
+//                Long::sum
 //        );
-////        JavaPairRDD<String, Long> wordsWithCount =
-////                splitted.mapToPair(
-////                        s -> new Tuple2<>( s, 1L)
-////                );
-////        JavaPairRDD<String, Long> collectedWords = wordsWithCount.reduceByKey (
-////                Long::sum
-////        );
-////        JavaRDD<String> dictionaryFile = sc.textFile( "russian.txt");
-////        JavaPairRDD<String, Long> dictionary =
-////                dictionaryFile.mapToPair(
-////                        s -> new Tuple2<>( s, 1L)
-////                );
-////        JavaPairRDD<String, Tuple2<Long, Long>> joinValue = dictionary.join( collectedWords);
-//        System.out.println( "BEGIN--------------------------------------------------------------------");
-//        System.out.println( "result="+splitted.collect());
-//        System.out.println( "END----------------------------------------------------------------------");
+//        JavaRDD<String> dictionaryFile = sc.textFile( "russian.txt");
+//        JavaPairRDD<String, Long> dictionary =
+//                dictionaryFile.mapToPair(
+//                        s -> new Tuple2<>( s, 1L)
+//                );
+//        JavaPairRDD<String, Tuple2<Long, Long>> joinValue = dictionary.join( collectedWords);
+        System.out.println( "BEGIN--------------------------------------------------------------------");
+        System.out.println( "result="+splitted.collect());
+        System.out.println( "END----------------------------------------------------------------------");
     }
 }
