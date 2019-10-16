c507e00a4aa385a8cb04955f94ec28e365cb90be
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-16 21:12:07 +0300

    Scripted auto-commit on change (2019-10-16 21:12:07) by gitwatch.sh

--- lab3/src/main/java/FlightStatApp.java
+++ lab3/src/main/java/FlightStatApp.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class FlightStatApp {
     public static void main(String[] args) throws Exception {
@@ -11,26 +13,8 @@ public class FlightStatApp {
             System.err.println("Usage: FlightStatApp <input path flights> <input path airport> <output path>");
             System.exit(-1);
         }
-        Job job = Job.getInstance();
+        SparkConf conf = new SparkConf(Hadoop ).setAppName(Hadoop "example");
+        JavaSparkContext sc = new JavaSparkContext(Hadoop conf);
 
-        job.setJarByClass(FlightStatApp.class);
-        job.setJobName("Flight stat count");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
-        FileOutputFormat.setOutputPath(job, new Path(args[2]));
-
-        job.setNumReduceTasks(2);
-
-        job.setMapOutputKeyClass(AirportFlightComp.class);
-        job.setMapOutputValueClass(Text.class);
-
-        job.setPartitionerClass(StatPartitioner.class);
-        job.setGroupingComparatorClass(StatGroupingComparator.class);
-
-        job.setReducerClass(StatReducer.class);
-
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
