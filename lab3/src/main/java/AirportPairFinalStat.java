ddff16e6852eb42341e4fc4e7293e392663cb6c6
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-17 00:56:03 +0300

    Scripted auto-commit on change (2019-10-17 00:56:03) by gitwatch.sh

--- lab3/src/main/java/AirportPairFinalStat.java
+++ lab3/src/main/java/AirportPairFinalStat.java
@@ -1,6 +1,8 @@
 import java.io.Serializable;
+import java.math.BigInteger;
 
 public class AirportPairFinalStat implements Serializable {
     private float maxDelay;
+    private BigInteger amount;
     
 }
