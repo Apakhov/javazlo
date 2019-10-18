cfcd0cf2a69e5921b7affb9e29cccb2e86e287cb
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-10-19 00:17:00 +0300

    Scripted auto-commit on change (2019-10-19 00:17:00) by gitwatch.sh

--- lab3/src/main/java/FlightStatApp.java
+++ lab3/src/main/java/FlightStatApp.java
@@ -8,34 +8,14 @@ import scala.Tuple2;
 import java.util.Map;
 
 public class FlightStatApp {
-    private enum FLIGHT_FIELDS = {
-            YEAR("YEAR"),
-            QUARTER("QUARTER"),
-            MONTH("MONTH"),
-            DAY_OF_MONTH("DAY_OF_MONTH"),
-            DAY_OF_WEEK("DAY_OF_WEEK"),
-            FL_DATE("FL_DATE"),
-            UNIQUE_CARRIER("UNIQUE_CARRIER"),
-            AIRLINE_ID("AIRLINE_ID"),
-            CARRIER("CARRIER"),
-            TAIL_NUM("TAIL_NUM"),
-            FL_NUM("FL_NUM"),
-            ORIGIN_AIRPORT_ID("ORIGIN_AIRPORT_ID"),
-            ORIGIN_AIRPORT_SEQ_ID("ORIGIN_AIRPORT_SEQ_ID"),
-            ORIGIN_CITY_MARKET_ID("ORIGIN_CITY_MARKET_ID"),
-            DEST_AIRPORT_ID("DEST_AIRPORT_ID"),
-            WHEELS_ON("WHEELS_ON"),
-            ARR_TIME("ARR_TIME"),
-            ARR_DELAY("ARR_DELAY"),
-            ARR_DELAY_NEW("ARR_DELAY_NEW"),
-            CANCELLED("CANCELLED"),
-            CANCELLATION_CODE("CANCELLATION_CODE"),
-            AIR_TIME("AIR_TIME"),
-            DISTANCE("DISTANCE");
+    public enum Constants {
+        NAME_1("Value1"),
+        NAME_2("Value2"),
+        NAME_3("Value3");
 
-            private String value;
+        private String value;
 
-        FLIGHT_FIELDS(String value) {
+        Constants(String value) {
             this.value = value;
         }
 
