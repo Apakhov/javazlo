64d1d09a87bc7475f13c3ad11c82655c84ce3a8f
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 00:14:29 +0300

    Scripted auto-commit on change (2019-11-02 00:14:29) by gitwatch.sh

--- lab4/src/main/java/StoreActor.java
+++ lab4/src/main/java/StoreActor.java
@@ -1,27 +1,43 @@
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
 public class StoreActor extends AbstractActor {
-    public static class StoreMessage {
-
-    }
-    public static class GetMessage {
+    public static class CreateStoreMessage {
 
     }
 
-    private Map<String, TestActor.ResultMessage[]> store = new HashMap<>();
+    public static class CreateStoreResponse {
+        private final String uuid;
+
+        public CreateStoreResponse(String uuid) {
+            this.uuid = uuid;
+        }
+
+        public String getUuid() {
+            return uuid;
+        }
+    }
+
+    private Map<String, ArrayList<TestActor.ResultMessage>> store = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestActor.ResultMessage.class, m -> {
-                    TestActor.ResultMessage[] res = store.get(m.getUUID());
-                    res.
-                    System.out.println("receive message! "+m.toString());
+                    ArrayList<TestActor.ResultMessage> res = store.get(m.getUUID());
+                    res.add(m);
+                    System.out.println("receive message! " + m.toString());
                 })
-                .match(GetMessage.class, req -> sender().tell(
-                        new StoreMessage(req.getKey(), store.get(req.getKey())), self())
+                .match(CreateStoreMessage.class, req -> {
+                    
+                            return sender().tell(
+                                    new StoreMessage(req.getKey(), store.get(req.getKey())), self()
+                            );
+                        }
                 ).build();
     }
 }
