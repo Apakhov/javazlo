8b67bc107ddb5e1b1b740821e94b2122730ffaec
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 02:27:44 +0300

    Scripted auto-commit on change (2019-11-02 02:27:44) by gitwatch.sh

--- lab4/src/main/java/JSCheckerApp.java
+++ lab4/src/main/java/JSCheckerApp.java
@@ -1,10 +1,12 @@
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
@@ -13,28 +15,47 @@ import java.util.concurrent.CompletionStage;
 
 import static java.lang.Thread.sleep;
 
-public class JSCheckerApp {
-    public static void main(String[] args) throws IOException, InterruptedException {
+public class JSCheckerApp extends AllDirectives {
+
+    // set up ActorSystem and other dependencies here
+    private final UserRoutes userRoutes;
+
+    public JSCheckerApp(ActorSystem system, ActorRef userRegistryActor) {
+        userRoutes = new UserRoutes(system, userRegistryActor);
+    }
+    //#main-class
+
+    public static void main(String[] args) throws Exception {
+        //#server-bootstrapping
         // boot up server using the route as defined below
-        ActorSystem system = ActorSystem.create("routes");
+        ActorSystem system = ActorSystem.create("helloAkkaHttpServer");
+
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
+        //#server-bootstrapping
 
+        ActorRef userRegistryActor = system.actorOf(UserRegistryActor.props(), "userRegistryActor");
+
+        //#http-server
         //In order to access all directives we need an instance where the routes are define.
-        TaskRouter app = new TaskRouter();
+        QuickstartServer app = new QuickstartServer(system, userRegistryActor);
 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
-                ConnectHttp.toHost("localhost", 8080), materializer);
+        http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8080), materializer);
 
-        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read(); // let it run until user presses return
+        System.out.println("Server online at http://localhost:8080/");
+        //#http-server
+    }
 
-        binding
-                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
-                .thenAccept(unbound -> system.terminate()); // and shutdown when done
-
-
-
+    //#main-class
+    /**
+     * Here you can define all the different routes you want to have served by this web server
+     * Note that routes might be defined in separated classes like the current case
+     */
+    protected Route createRoute() {
+        return userRoutes.routes();
     }
 }
+//#main-class
+
+
