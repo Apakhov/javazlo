f306dcd9daf876fc16d2db599f50d58ca3e54d2e
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-01 23:52:03 +0300

    Scripted auto-commit on change (2019-11-01 23:52:03) by gitwatch.sh

--- lab4/src/main/java/HttpRouterActor.java
+++ lab4/src/main/java/HttpRouterActor.java
@@ -14,28 +14,6 @@ import java.util.concurrent.CompletionStage;
 
 public class HttpRouterActor extends AllDirectives {
 
-    public static void main(String[] args) throws Exception {
-        // boot up server using the route as defined below
-        ActorSystem system = ActorSystem.create("routes");
-
-        final Http http = Http.get(system);
-        final ActorMaterializer materializer = ActorMaterializer.create(system);
-
-        //In order to access all directives we need an instance where the routes are define.
-        HttpRouterActor app = new HttpRouterActor();
-
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
-                ConnectHttp.toHost("localhost", 8080), materializer);
-
-        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read(); // let it run until user presses return
-
-        binding
-                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
-                .thenAccept(unbound -> system.terminate()); // and shutdown when done
-    }
-
     private Route createRoute() {
         return concat(
                 path("test", () ->
--- lab4/src/main/java/JSCheckerApp.java
+++ lab4/src/main/java/JSCheckerApp.java
@@ -1,27 +1,38 @@
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.pattern.PatternsCS;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.util.concurrent.CompletionStage;
 
 public class JSCheckerApp {
     public static void main(String[] args) {
-        ActorSystem system = ActorSystem.create("test");
-        ActorRef storeActor = system.actorOf(
-                Props.create(StoreActor.class)
-        );
-        ActorRef testActor = system.actorOf(
-                Props.create(TestActor.class)
-        );
-        testActor.tell(
-                new TestActor.TestMessage(
-                        "test",
-                        "var divideFn = function(a,b) { return a*b }",
-                        "divideFn",
-                        new Object[]{2, 1},
-                        "2"
-                ),
-                ActorRef.noSender());
+        // boot up server using the route as defined below
+        ActorSystem system = ActorSystem.create("routes");
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        //In order to access all directives we need an instance where the routes are define.
+        HttpRouterActor app = new HttpRouterActor();
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
+                ConnectHttp.toHost("localhost", 8080), materializer);
+
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read(); // let it run until user presses return
+
+        binding
+                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
     }
 }
