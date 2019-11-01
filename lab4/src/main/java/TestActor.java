6a1f9e8aa9f5ea9bbe0e30820b09fad813e19f04
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-01 22:26:31 +0300

    Scripted auto-commit on change (2019-11-01 22:26:31) by gitwatch.sh

--- lab4/src/main/java/TestActor.java
+++ lab4/src/main/java/TestActor.java
@@ -2,29 +2,11 @@ import akka.actor.AbstractActor;
 
 public class TestActor extends AbstractActor {
     public static class TestMessage {
-        public TestMessage(String key, String value) {
-            this.sourceCode = key;
-            this.value = value;
-        }
 
-        public String getSourceCode() {
-            return sourceCode;
-        }
 
-        public String getValue() {
-            return value;
-        }
-
-        @Override
-        public String toString() {
-            return "StoreMessage{" +
-                    "key='" + sourceCode + '\'' +
-                    ", value='" + value + '\'' +
-                    '}';
-        }
 
         private final String sourceCode;
-        private final String value;
+        private final String ;
     }
 
     @Override
