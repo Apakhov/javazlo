4faccf73d706de7e8ab768ac574dcb256e97a1f5
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 00:45:32 +0300

    Scripted auto-commit on change (2019-11-02 00:45:32) by gitwatch.sh

--- lab4/src/main/java/StoreActor.java
+++ lab4/src/main/java/StoreActor.java
@@ -1,4 +1,5 @@
 import akka.actor.AbstractActor;
+import akka.japi.Pair;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.ArrayList;
@@ -8,7 +9,16 @@ import java.util.UUID;
 
 public class StoreActor extends AbstractActor {
     public static class CreateStoreMessage {
+        private final Integer testsAmount;
 
+        public CreateStoreMessage(Integer testsAmount) {
+
+            this.testsAmount = testsAmount;
+        }
+
+        public Integer getTestsAmount() {
+            return testsAmount;
+        }
     }
 
     public static class CreateStoreResponse {
@@ -23,23 +33,81 @@ public class StoreActor extends AbstractActor {
         }
     }
 
-    private Map<UUID, ArrayList<TestActor.ResultMessage>> store = new HashMap<>();
+    public static class GetResultMessage {
+        private final UUID uuid;
+
+        public GetResultMessage(UUID uuid) {
+            this.uuid = uuid;
+        }
+
+        public UUID getUuid() {
+            return uuid;
+        }
+    }
+
+    public static class GetResultResponse {
+        private final Integer testsAmount;
+        private final ArrayList<TestResult> results;
+
+        public GetResultResponse(Integer testsAmount, ArrayList<TestResult> results){
+            this.testsAmount = testsAmount;
+            this.results = results;
+        }
+
+        public Integer getTestsAmount() {
+            return testsAmount;
+        }
+
+        public ArrayList<TestResult> getResults() {
+            return results;
+        }
+    }
+
+    public static class TestResult {
+        private final TestActor.ResultMessage m;
+        public TestResult(TestActor.ResultMessage m){
+            this.m = m;
+        }
+
+        public String getExpectedRes() {
+            return m.getExpectedRes();
+        }
+
+        public String getActualRes() {
+            return m.getActualRes();
+        }
+
+        public boolean isOK() {
+            return m.isOK();
+        }
+
+        public String getError() {
+            return m.getError().toString();
+        }
+    }
+
+    private Map<UUID, Pair<Integer, ArrayList<TestResult>>> store = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestActor.ResultMessage.class, m -> {
-                    ArrayList<TestActor.ResultMessage> res = store.get(m.getUUID());
-                    res.add(m);
-                    System.out.println("receive message! " + m.toString());
+                    Pair<Integer, ArrayList<TestResult>> res = store.get(m.getUUID());
+                    res.second().add(new TestResult(m));
                 })
                 .match(CreateStoreMessage.class, req -> {
-                            UUID uuid = UUID.randomUUID();
-                            store.put(uuid, new ArrayList<>());
-                            sender().tell(
-                                    new CreateStoreResponse(uuid), self()
-                            );
-                        }
-                ).build();
+                    UUID uuid = UUID.randomUUID();
+                    store.put(uuid, new Pair<>(req.getTestsAmount(), new ArrayList<>()));
+                    sender().tell(
+                            new CreateStoreResponse(uuid), self()
+                    );
+                })
+                .match(GetResultMessage.class, req -> {
+                    Pair<Integer, ArrayList<TestResult>> tests = store.get(req.getUuid());
+                    sender().tell(
+                            new GetResultResponse(), self()
+                    );
+                })
+                .build();
     }
 }
