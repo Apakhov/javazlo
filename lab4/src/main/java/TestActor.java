ccb893172dedb1ae636393ca968d5d2edd425d3f
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-01 22:48:25 +0300

    Scripted auto-commit on change (2019-11-01 22:48:25) by gitwatch.sh

--- lab4/src/main/java/TestActor.java
+++ lab4/src/main/java/TestActor.java
@@ -80,7 +80,7 @@ public class TestActor extends AbstractActor {
             engine.eval(m.getSourceCode());
             Invocable invocable = (Invocable) engine;
             invocable.invokeFunction(m.getFuncName(), (Object[]) m.getArgs());
-            String res = invocable.invokeFunction(functionName, params).toString();
+            String res = invocable.invokeFunction(m.getFuncName(), m.getArgs()).toString();
         } catch (Exception e){
             return new ResultMessage(e, m.getExpectedRes());
         }
