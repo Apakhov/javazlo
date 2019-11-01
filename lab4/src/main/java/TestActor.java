6f42179dd70847ee8b5c2f205bcac8c2d0a61a18
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-01 23:59:56 +0300

    Scripted auto-commit on change (2019-11-01 23:59:56) by gitwatch.sh

--- lab4/src/main/java/TestActor.java
+++ lab4/src/main/java/TestActor.java
@@ -1,5 +1,9 @@
 import akka.actor.AbstractActor;
+import akka.actor.OneForOneStrategy;
+import akka.actor.SupervisorStrategy;
+import akka.japi.pf.DeciderBuilder;
 import akka.japi.pf.ReceiveBuilder;
+import scala.concurrent.duration.Duration;
 
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
@@ -107,5 +111,20 @@ public class TestActor extends AbstractActor {
                     test(m), self()
                 ))
                 .build();
+    }
+
+    private static final int MAX_RETRIES = 10;
+
+    private static SupervisorStrategy strategy =
+            new OneForOneStrategy(MAX_RETRIES,
+                    Duration.create("1 minute"),
+                    DeciderBuilder.
+                            match(ArithmeticException.class, e -> resume()).
+                            match(NullPointerException.class, e -> restart()).
+                            match(IllegalArgumentException.class, e -> stop()).
+                            matchAny(o -> escalate()).build());
+    @Override
+    public SupervisorStrategy supervisorStrategy() {
+        return strategy;
     }
 }
