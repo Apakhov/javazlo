968a62be17afae8014bbf31f268d6c797ac4affa
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-01 22:21:18 +0300

    Scripted auto-commit on change (2019-11-01 22:21:18) by gitwatch.sh

--- lab4/src/main/java/TestActor.java
+++ lab4/src/main/java/TestActor.java
@@ -1,2 +1,8 @@
-public class TestActor {
+import akka.actor.AbstractActor;
+
+public class TestActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
