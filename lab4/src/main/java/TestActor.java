f4419ba11c0c071a8687690f31d1276ded87a5c1
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-01 22:31:37 +0300

    Scripted auto-commit on change (2019-11-01 22:31:37) by gitwatch.sh

--- lab4/src/main/java/TestActor.java
+++ lab4/src/main/java/TestActor.java
@@ -1,18 +1,46 @@
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class TestActor extends AbstractActor {
     public static class TestMessage {
+        public String getSourceCode() {
+            return sourceCode;
+        }
 
+        public String getFuncName() {
+            return funcName;
+        }
 
+        public String[] getArgs() {
+            return args;
+        }
+
+        public String getExpectedRes() {
+            return expectedRes;
+        }
 
         private final String sourceCode;
         private final String funcName;
         private final String[] args;
         private final String expectedRes;
+
+        public TestMessage(String sourceCode, String funcName, String[] args, String expectedRes) {
+            this.sourceCode = sourceCode;
+            this.funcName = funcName;
+            this.args = args;
+            this.expectedRes = expectedRes;
+        }
     }
 
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                    store.put(m.getKey(), m.getValue());
+                    System.out.println("receive message! "+m.toString());
+                })
+                .match(GetMessage.class, req -> sender().tell(
+                        new StoreMessage(req.getKey(), store.get(req.getKey())), self())
+                ).build();
     }
 }
