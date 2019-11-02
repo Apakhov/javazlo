968382a2a6cdb24365372fa7b5aafc2955e00908
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 17:14:22 +0300

    Scripted auto-commit on change (2019-11-02 17:14:22) by gitwatch.sh

--- lab4/src/main/java/TestActor.java
+++ lab4/src/main/java/TestActor.java
@@ -12,59 +12,7 @@ import java.util.UUID;
 public class TestActor extends AbstractActor {
     LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
-    public static class ResultMessage {
-        @Override
-        public String toString() {
-            return "ResultMessage{" +
-                    "expectedRes='" + expectedRes + '\'' +
-                    ", actualRes='" + actualRes + '\'' +
-                    ", isOK=" + isOK +
-                    ", error=" + error +
-                    '}';
-        }
 
-        ResultMessage(UUID uuid, String expectedRes, String actualRes) {
-            this.uuid = uuid;
-            this.expectedRes = expectedRes;
-            this.actualRes = actualRes;
-            this.isOK = expectedRes.equals(actualRes);
-            this.error = null;
-        }
-
-        ResultMessage(UUID uuid, String expectedRes, Exception error) {
-            this.uuid = uuid;
-            this.error = error;
-            this.expectedRes = expectedRes;
-            this.actualRes = "";
-            this.isOK = false;
-        }
-
-        public UUID getUUID() {
-            return uuid;
-        }
-
-        public String getExpectedRes() {
-            return expectedRes;
-        }
-
-        public String getActualRes() {
-            return actualRes;
-        }
-
-        public boolean isOK() {
-            return isOK;
-        }
-
-        public Exception getError() {
-            return error;
-        }
-
-        private final UUID uuid;
-        private final String expectedRes;
-        private final String actualRes;
-        private final boolean isOK;
-        private final Exception error;
-    }
 
     private ResultMessage test(TestMsg m) {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
