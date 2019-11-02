c0ed0b756692ac45678175b35aeb3a45ee322e7a
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 10:06:26 +0300

    Scripted auto-commit on change (2019-11-02 10:06:26) by gitwatch.sh

--- lab4/src/main/java/RouterActor.java
+++ lab4/src/main/java/RouterActor.java
@@ -1,10 +1,18 @@
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 
 public class RouterActor extends AbstractActor {
+    private final ActorRef storeActor;
+
     @Override
     public void preStart() {
-        getContext().actorOf(StoreActor.props(), "second");
+        storeActor = getContext().actorOf(StoreActor.props(), "second");
     }
 
-    
+    testPerformerActor = getContext().actorOf(
+new RoundRobinPool(5)
+.withSupervisorStrategy(strategy)
+.props(Props.create(TestPerformerActor.class, logResultsActor)),
+            "routerForTests"
+            );
 }
