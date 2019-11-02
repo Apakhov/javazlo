9145e217f4aeb24dc017c18315b38542427293ee
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 09:42:11 +0300

    Scripted auto-commit on change (2019-11-02 09:42:11) by gitwatch.sh

--- lab4/src/main/java/QuickstartServer.java
+++ lab4/src/main/java/QuickstartServer.java
@@ -31,7 +31,7 @@ public class QuickstartServer extends AllDirectives {
         //#server-bootstrapping
 
         ActorRef userRegistryActor = system.actorOf(UserRegistryActor.props(), "userRegistryActor");
-        ActorRef storeActor = system.actorOf(StoreActor.props(), "userRegistryActor");
+        ActorRef storeActor = system.actorOf(StoreActor.props(), "storeActor");
 
         //#http-server
         //In order to access all directives we need an instance where the routes are define.
