47af3a9ee0d639a046db9de230826ecba846f9ec
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 11:46:33 +0300

    Scripted auto-commit on change (2019-11-02 11:46:33) by gitwatch.sh

--- lab4/src/main/java/Test.java
+++ lab4/src/main/java/Test.java
@@ -1,2 +1,44 @@
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Test {
+    private String packageID;
+    private String testName;
+    private String expectedResult;
+    private Object[] params;
+
+    public Test() {
+        this.packageID = "";
+        this.testName = "";
+        this.expectedResult = "";
+        this.params = new Object[]{};
+    }
+
+    public Test(String name, String expectedResult, Object[] params) {
+        this.packageID = "";
+        this.testName = name;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
+
+    public void setPackageID(String packageID) {
+        this.packageID = packageID;
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
 }
