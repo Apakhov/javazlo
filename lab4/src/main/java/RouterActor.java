108c8896ef73876445b3fd2ac6d9ebc228e30e28
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 15:37:31 +0300

    Scripted auto-commit on change (2019-11-02 15:37:31) by gitwatch.sh

--- lab4/src/main/java/RouterActor.java
+++ lab4/src/main/java/RouterActor.java
@@ -36,12 +36,13 @@ public class RouterActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestActor.TestMessage.class, req -> {
-                    log.info("Receive ! {}",req.toString());
+                    log.info("Receive ! {}", req.toString());
                     testPool.tell(req, storeActor);
                 })
-                .match(StoreActor.GetResultMessage.class, req ->
-                    storeActor.forward(req, getContext())
-                )
+                .match(StoreActor.GetResultMessage.class, req -> {
+                            log.info("Receive ! {}", req.toString());
+                            storeActor.forward(req, getContext());
+                        })
                 .matchAny(o -> log.info(o.toString() + o.getClass()))
                 .build();
     }
