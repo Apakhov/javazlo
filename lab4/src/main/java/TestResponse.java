8c57c7c9c4d49d2482f6ea75ec763dca7d4d8fab
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 18:34:33 +0300

    Scripted auto-commit on change (2019-11-02 18:34:33) by gitwatch.sh

--- lab4/src/main/java/TestResponse.java
+++ lab4/src/main/java/TestResponse.java
@@ -1,5 +1,8 @@
 public class TestResponse extends TestMetaInfo {
+    private TestResult[] testResults;
+
     TestResponse(ResMsg msg){
         super(msg.result.first());
+        msg.result.second().toArray(testResults);
     }
 }
