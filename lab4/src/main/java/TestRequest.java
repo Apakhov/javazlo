47034d309a849ca04c920361f61dd15e06a45b67
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 11:47:08 +0300

    Scripted auto-commit on change (2019-11-02 11:47:08) by gitwatch.sh

--- lab4/src/main/java/TestRequest.java
+++ lab4/src/main/java/TestRequest.java
@@ -1,2 +1,21 @@
-public class TestRequest {
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class TestRequest extends TestMetaInfo {
+    private Test[] tests;
+
+    public TestRequest() {
+        super();
+        this.tests = new Test[]{};
+    }
+
+    public TestRequest(String packageID, String jsCode, String functionName, Test[] tests) {
+        super(packageID, jsCode, functionName);
+        this.tests = tests;
+    }
+
+    public Test[] getTests() {
+        return tests;
+    }
 }
