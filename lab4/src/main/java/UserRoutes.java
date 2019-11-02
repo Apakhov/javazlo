26ce85870927de56dda70f135e6d785e9f74f593
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 11:35:59 +0300

    Scripted auto-commit on change (2019-11-02 11:35:59) by gitwatch.sh

--- lab4/src/main/java/UserRoutes.java
+++ lab4/src/main/java/UserRoutes.java
@@ -88,6 +88,37 @@ public class UserRoutes extends AllDirectives {
         }
     }
     @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+    public class TestMetaInfo {
+        private String packageID;
+        private String jsCode;
+        private String functionName;
+
+        public TestMetaInfo() {
+            this.packageID = "";
+            this.jsCode = "";
+            this.functionName = "";
+        }
+
+        public TestMetaInfo(String packageID, String jsCode, String functionName) {
+            this.packageID = packageID;
+            this.jsCode = jsCode;
+            this.functionName = functionName;
+        }
+
+        public String getPackageID() {
+            return packageID;
+        }
+
+        public String getJsCode() {
+            return jsCode;
+        }
+
+        public String getFunctionName() {
+            return functionName;
+        }
+    }
+    
+    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
     public class TestRequest extends TestMetaInfo {
         private Test[] tests;
 
