f8b2a2cec8f267efc23553e3fa22c6ec360394d3
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 17:26:39 +0300

    Scripted auto-commit on change (2019-11-02 17:26:39) by gitwatch.sh

--- lab4/src/main/java/TestActor.java
+++ lab4/src/main/java/TestActor.java
@@ -7,14 +7,13 @@ import akka.japi.pf.ReceiveBuilder;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import java.util.UUID;
 
 public class TestActor extends AbstractActor {
     LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
 
 
-    private ResultMessage test(TestMsg m) {
+    private TestResult test(TestMsg m) {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         try {
             engine.eval(m.testMetaInfo.getSourceCode());
@@ -23,10 +22,10 @@ public class TestActor extends AbstractActor {
             String res = invocable.invokeFunction(m.testMetaInfo.getFuncName(), args).toString();
             log.info("Successful test: res: " + res.toString() + ", expected:" + m.testCase.getExpectedResult());
             //throw new Exception("Successful test: res: " + res.toString() + ", expected:" + m.getExpectedRes().toString() + m.uuid);
-            return new ResultMessage(m.testMetaInfo.getUUID(), m.testCase.getExpectedResult(), res);
+            return new TestResult(m.testMetaInfo.getUUID(), m.testCase.getExpectedResult(), res);
         } catch (Exception e) {
             System.out.println("exception occurred: " + e.toString());
-            return new ResultMessage(m.testMetaInfo.getUUID(), m.testCase.getExpectedResult(), e);
+            return new TestResult(m.testMetaInfo.getUUID(), m.testCase.getExpectedResult(), e);
         }
     }
 
--- lab4/src/main/java/TestResult.java
+++ lab4/src/main/java/TestResult.java
@@ -1,53 +1,14 @@
-public class ResultMessage {
-    @Override
-    public String toString() {
-        return "ResultMessage{" +
-                "expectedRes='" + expectedRes + '\'' +
-                ", actualRes='" + actualRes + '\'' +
-                ", isOK=" + isOK +
-                ", error=" + error +
-                '}';
-    }
+import java.util.UUID;
 
-    ResultMessage(UUID uuid, String expectedRes, String actualRes) {
+public class TestResult extends TestCase {
+    private UUID uuid;
+
+    public TestCase(UUID uuid) {
+        super();
         this.uuid = uuid;
-        this.expectedRes = expectedRes;
-        this.actualRes = actualRes;
-        this.isOK = expectedRes.equals(actualRes);
-        this.error = null;
     }
 
-    ResultMessage(UUID uuid, String expectedRes, Exception error) {
-        this.uuid = uuid;
-        this.error = error;
-        this.expectedRes = expectedRes;
-        this.actualRes = "";
-        this.isOK = false;
+    public TestCase(UUID uuid, String name, String expectedResult, Object[] args) {
+        super(name, expectedResult, args);
     }
-
-    public UUID getUUID() {
-        return uuid;
-    }
-
-    public String getExpectedRes() {
-        return expectedRes;
-    }
-
-    public String getActualRes() {
-        return actualRes;
-    }
-
-    public boolean isOK() {
-        return isOK;
-    }
-
-    public Exception getError() {
-        return error;
-    }
-
-    private final UUID uuid;
-    private final String expectedRes;
-    private final String actualRes;
-    private final boolean isOK;
-    private final Exception error;
 }
