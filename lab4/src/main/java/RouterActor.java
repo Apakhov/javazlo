1970e7cb0fd5e9c32f934a041df22ddb02a76c2d
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 10:11:45 +0300

    Scripted auto-commit on change (2019-11-02 10:11:45) by gitwatch.sh

--- lab4/src/main/java/RouterActor.java
+++ lab4/src/main/java/RouterActor.java
@@ -1,10 +1,22 @@
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.OneForOneStrategy;
+import akka.actor.SupervisorStrategy;
+import akka.japi.pf.DeciderBuilder;
 import akka.routing.RoundRobinPool;
+import scala.concurrent.duration.Duration;
 
 public class RouterActor extends AbstractActor {
     private final ActorRef storeActor;
     private final ActorRef testPool;
+    private static SupervisorStrategy strategy =
+            new OneForOneStrategy(MAX_RETRIES,
+                    Duration.create("1 minute"),
+                    DeciderBuilder.
+                            match(ArithmeticException.class, e -> resume()).
+                            match(NullPointerException.class, e -> restart()).
+                            match(IllegalArgumentException.class, e -> stop()).
+                            matchAny(o -> escalate()).build());
 
     public RouterActor() {
         super();
