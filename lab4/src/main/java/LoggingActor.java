e3018ee856a70f972354447f81d889c637077481
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 19:12:47 +0300

    Scripted auto-commit on change (2019-11-02 19:12:46) by gitwatch.sh

--- lab4/src/main/java/LoggingActor.java
+++ lab4/src/main/java/LoggingActor.java
@@ -1,6 +1,12 @@
+import akka.actor.AbstractActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 
-public class LoggingActor {
+public class LoggingActor extends AbstractActor {
     LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
