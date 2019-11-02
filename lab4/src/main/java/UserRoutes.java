e56129a63dd187606eb2eef7e6a34a719512b118
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 11:46:30 +0300

    Scripted auto-commit on change (2019-11-02 11:46:30) by gitwatch.sh

--- lab4/src/main/java/UserRoutes.java
+++ lab4/src/main/java/UserRoutes.java
@@ -88,47 +88,7 @@ public class UserRoutes extends AllDirectives {
         }
     }
 
-    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-    public class Test {
-        private String packageID;
-        private String testName;
-        private String expectedResult;
-        private Object[] params;
 
-        public Test() {
-            this.packageID = "";
-            this.testName = "";
-            this.expectedResult = "";
-            this.params = new Object[]{};
-        }
-
-        public Test(String name, String expectedResult, Object[] params) {
-            this.packageID = "";
-            this.testName = name;
-            this.expectedResult = expectedResult;
-            this.params = params;
-        }
-
-        public void setPackageID(String packageID) {
-            this.packageID = packageID;
-        }
-
-        public String getPackageID() {
-            return packageID;
-        }
-
-        public String getTestName() {
-            return testName;
-        }
-
-        public String getExpectedResult() {
-            return expectedResult;
-        }
-
-        public Object[] getParams() {
-            return params;
-        }
-    }
     @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
     public class TestRequest extends TestMetaInfo {
         private Test[] tests;
