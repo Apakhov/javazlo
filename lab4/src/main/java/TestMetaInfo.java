bab55dda06b346979178f76b5911be82f065c506
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-02 11:46:09 +0300

    Scripted auto-commit on change (2019-11-02 11:46:09) by gitwatch.sh

--- lab4/src/main/java/TestMetaInfo.java
+++ lab4/src/main/java/TestMetaInfo.java
@@ -1,2 +1,33 @@
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class TestMetaInfo {
+    private String packageID;
+    private String jsCode;
+    private String functionName;
+
+    public TestMetaInfo() {
+        this.packageID = "";
+        this.jsCode = "";
+        this.functionName = "";
+    }
+
+    public TestMetaInfo(String packageID, String jsCode, String functionName) {
+        this.packageID = packageID;
+        this.jsCode = jsCode;
+        this.functionName = functionName;
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
+
+    public String getJsCode() {
+        return jsCode;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
 }
