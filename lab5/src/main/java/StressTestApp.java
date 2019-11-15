91ac57f4ac81eb4fad2b4d5a7e47a0053c2dc6e9
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-15 23:14:51 +0300

    Scripted auto-commit on change (2019-11-15 23:14:51) by gitwatch.sh

---
+++ lab5/src/main/java/StressTestApp.java
@@ -0,0 +1,45 @@
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import javafx.util.Pair;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
+public class StressTestApp {
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = Flow.of(HttpRequest.class)
+                .map(req -> {
+                    String url = req.getUri().query().get("testURL").orElse("");
+                    String rawCount = req.getUri().query().get("count").orElse("1");
+                    int count = 1;
+                    if (rawCount.matches("-?\\d+")){
+                        count = Integer.parseInt(rawCount);
+                    }
+
+                    return new Pair(url,count);
+                    
+                }).mapAsync();
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdownwhen done
+    }
+}
