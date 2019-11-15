71544d9bac8bf2401420024db92e8fb9561c6df1
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-16 00:50:16 +0300

    Scripted auto-commit on change (2019-11-16 00:50:16) by gitwatch.sh

--- lab5/src/main/java/StressTestApp.java
+++ lab5/src/main/java/StressTestApp.java
@@ -61,7 +61,7 @@ public class StressTestApp {
                                 return f;
                             });
                     Sink<Long, CompletionStage<Long>> fold = Sink.fold(0L, Long::sum);
-                    sink = flow.toMat(fold, )
+                    Sink<TestRequest, CompletionStage<Long>> sink = flow.toMat(fold, Keep.right());
                     Source<Long, NotUsed> counted = Source.from(Collections.singletonList(p)).viaMat(flow, Keep.right());
                     return Source.from(Collections.singletonList(p))
                             .toMat(flow
