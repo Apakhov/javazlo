a76b3fa5f9b1717ddbd22a4326ae25d4d3e38b77
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-16 01:25:19 +0300

    Scripted auto-commit on change (2019-11-16 01:25:19) by gitwatch.sh

--- lab5/src/main/java/StoreActor.java
+++ lab5/src/main/java/StoreActor.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.UUID;
 
 public class StoreActor extends AbstractActor {
-    LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
     private Map<String, Pair<Integer, Long>> store = new HashMap<>();
 
     @Override
@@ -23,18 +22,9 @@ public class StoreActor extends AbstractActor {
                     else
                         sender().tell(StoreResp.noInfo(), self());
                 })
-                .match(GetResMsg.class, req -> {
-                    log.info("store get res with: " + req.uuid);
-                    if (!store.containsKey(req.uuid)) {
-                        sender().tell(
-                                new ResMsg(), self()
-                        );
-                        return;
-                    }
-                    log.info(req.uuid.toString());
-                    sender().tell(
-                            new ResMsg(store.get(req.uuid)), self()
-                    );
+                .match(TestResult.class, m -> {
+                    if (!store.containsKey(m.req.url) || store.get(m.req.url).first() <= m.req.count)
+                        store.put(m.req.url, new Pair<>(m.req.count, m.timing / m.req.count));
                 })
                 .build();
     }
