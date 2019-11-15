5e25bc848de28a0640e209d591482cb536542ceb
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-15 23:47:09 +0300

    Scripted auto-commit on change (2019-11-15 23:47:09) by gitwatch.sh

--- lab5/src/main/java/StressTestApp.java
+++ lab5/src/main/java/StressTestApp.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Future;
 
 public class StressTestApp {
     public static void main(String[] args) throws IOException {
