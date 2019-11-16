ac1ddca8bbb09d0ef973f50806cd83f0ad8c9741
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2019-11-16 10:56:21 +0300

    Scripted auto-commit on change (2019-11-16 10:56:21) by gitwatch.sh

--- lab5/src/main/java/StoreActor.java
+++ lab5/src/main/java/StoreActor.java
@@ -29,6 +29,7 @@ public class StoreActor extends AbstractActor {
                         sender().tell(StoreResp.noInfo(), self());
                 })
                 .match(TestResult.class, m -> {
+                    System.out.println(m.req.url + "!!!"+m.req.count);
                     if (!store.containsKey(m.req.url) || store.get(m.req.url).first() <= m.req.count)
                         store.put(m.req.url, new Pair<>(m.req.count, m.timing / m.req.count));
                 })
